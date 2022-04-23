345029ae58d76abef854c9a995684fa1bc3a8228
SFort <github@ssf.tf>
2022-04-23 15:33:20 +0200

    change -o entries to be in a single line; close #1

--- steam_dmenu.sh
+++ steam_dmenu.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 run="dmenu -i"
 pkill=0
+printfmt='%s  :%s\n'
 path="$HOME/.local/share/Steam/steamapps/"
 while getopts "ohd:l:" arg; do
 	case "$arg" in
@@ -13,9 +14,11 @@ while getopts "ohd:l:" arg; do
 	-d Custom dmenu command. Default: $run
 	-l Steam appmanifest location. Default : $path
 
+	any additional parameters given will be considered as static options
+	and should be entered as pairs of "Name" "ID"
 PIPE BEHAVIOUR
-	Piping out will output "name" "id"...
-	Piping into the program takes "name" "id"...
+	Piping out will output "name"  :"id"...
+	Piping into the program takes "name"  :"id"...
 NOTE
 	Don't use relative paths
 	Paths must be folders and end with /
@@ -23,12 +26,13 @@ NOTE
 	esac
 done
 shift $(($OPTIND -1))
-
+test -t 0
+test $? -ne 0 && test $# -eq 0 && printfmt='%s\n' && set -- "$@" "`cat`"
 test $# -eq 0 && for arg in ${path}appmanifest_*.acf; do
 line="`cat $arg`";
 nam="`echo "$line"|tr '\n\t' ' '|sed 's/.*"name"[^"]*"\([^"]*\).*/\1/'|tr ' ' '_'`"
 set -- "$@" "$nam" "`echo "$line"|tr '\n\t' ' '|sed 's/.*"appid"[^"]*"\([^"]*\).*/\1/'`" 
 done
-test $pkill -eq 1 && printf '%s\n' "$@" && exit
-run=`printf '%s  :%s\n' "$@" | $run | sed 's/.*:\(.*\)/\1/'`
+test $pkill -eq 1 && printf "$printfmt" "$@" && exit
+run=`printf "$printfmt" "$@" | $run | cut -d: -f2`
 test -n "$run" && xdg-open "steam://run/$run"
